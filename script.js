6b766e480bd1dbcfbcb5d9039bf7e455dbc5e15b
John Furtado <jf8846@gmail.com>
2019-04-14 18:07:50 -0400

    changed imageSrc url

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var MainController = function($scope) {
     firstName: "Dwight",
     lastName: "Schrute",
     imageSrc:
-      "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj54J2FytDhAhXWFzQIHTqQArAQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FDwight_Schrute&psig=AOvVaw0bT1gpjuKp1-p6W72O2hb4&ust=1555365466797885"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzj9UIzG9lDCRqL-3_kuwjgmCAMwxrW4Ikeg08nI4wgo8eZrjB"
   };
 
   $scope.message = "Hello, World!";
