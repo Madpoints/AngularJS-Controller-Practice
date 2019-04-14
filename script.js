c032e58e81839913fe9a7d04dd95a258fd30bb78
John Furtado <jf8846@gmail.com>
2019-04-14 17:59:00 -0400

    person object added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var MainController = function($scope) {
+  var person = {
+    firstName: "Dwight",
+    lastName: "Schrute",
+    imageSrc:
+      "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj54J2FytDhAhXWFzQIHTqQArAQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FDwight_Schrute&psig=AOvVaw0bT1gpjuKp1-p6W72O2hb4&ust=1555365466797885"
+  };
+
   $scope.message = "Hello, World!";
+  $scope.person = person;
 };
